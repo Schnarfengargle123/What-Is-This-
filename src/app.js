cdf14b314006d3dedcf3c0d9f7d78fffa20330fa
Schnarfengargle123 <schnarfengargle123@gmail.com>
2021-03-11 12:06:39 +0000

    Moved answer functions into respective question function body

--- src/app.js
+++ src/app.js
@@ -7,18 +7,6 @@ let option2 = document.getElementById("option2");
 let option3 = document.getElementById("option3");
 let option4 = document.getElementById("option4");
 
-// Incorrect Answer
-document.getElementById("option1", "option2", "option4").onclick = function () {
-    alert("Not quite, try again!");
-    question1();
-}
-
-// Correct Answer
-document.getElementById("option3").onclick = function () {
-  alert("Correct!");
-  question2();
-};
-
 // Question Rounds
 function question1() {
   image1.src = "./assets/images/seal.jpg";
@@ -31,6 +19,51 @@ function question1() {
   setTimeout(function () {
     alert("Question 1!");
   }, 1500);
+
+  // Incorrect Answer
+  document.getElementById(
+    "option1",
+    "option2",
+    "option4"
+  ).onclick = function () {
+    alert("Not quite, try again!");
+    question1();
+  };
+
+  // Correct Answer
+  document.getElementById("option3").onclick = function () {
+    alert("Correct!");
+    question2();
+  };
+}
+
+function question2() {
+  image1.src = "./assets/images/tokyo_tower.jpg";
+
+  option1.innerHTML = "Eiffel Tower";
+  option2.innerHTML = "Shanghai Tower";
+  option3.innerHTML = "Blackpool Tower";
+  option4.innerHTML = "Tokyo Tower";
+
+  setTimeout(function () {
+    alert("Question 1!");
+  }, 1500);
+
+  // Incorrect Answer
+  document.getElementById(
+    "option1",
+    "option2",
+    "option3"
+  ).onclick = function () {
+    alert("Not quite, try again!");
+    question1();
+  };
+
+  // Correct Answer
+  document.getElementById("option4").onclick = function () {
+    alert("Correct!");
+    question3();
+  };
 }
 
 question1();
